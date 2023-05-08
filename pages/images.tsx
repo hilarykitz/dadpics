10b05e54d279dc67922f9941124e79bee97a3ab1
Hilary Kitz <hilarykitz@gmail.com>
2023-05-08 12:45:35 +0100

    chore(fetcher): use basic fetch

--- pages/images.tsx
+++ pages/images.tsx
@@ -1,16 +1,25 @@
-"use client";
-import useSWR from "swr";
+import { useEffect, useState } from "react";
 
 import { handler } from "./api/handler";
 
 const Images = () => {
-  const resp = useSWR("/api/getFiles", handler);
-  const data = resp?.data;
+  const [images, setImages] = useState<string[]>();
+
+  const fetchImages = async () => {
+    const resp = await handler("/api/getFiles");
+    setImages(resp);
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+  // const resp = useSWR("/api/getFiles", handler);
+  // const data = resp?.data;
 
   return (
     <section className="flex flex-col justify-between min-h-screen p24">
-      {data &&
-        data.map((path: string) => (
+      {images?.length &&
+        images.map((path: string) => (
           <div className="imageWrapper" key={path}>
             <img src={path} alt="" />
           </div>
