e2bba26335d256b128a71fb6d0dfa462e8b3b8ba
Hilary Kitz <hilarykitz@gmail.com>
2023-05-08 12:33:35 +0100

    chore(build): tweak

--- pages/images.tsx
+++ pages/images.tsx
@@ -4,15 +4,17 @@ import useSWR from "swr";
 import { handler } from "./api/handler";
 
 const Images = () => {
-  const { data } = useSWR("/api/getFiles", handler);
+  const resp = useSWR("/api/getFiles", handler);
+  const data = resp?.data;
 
   return (
     <section className="flex flex-col justify-between min-h-screen p24">
-      {data?.map((path: string) => (
-        <div className="imageWrapper">
-          <img src={path} alt="" />
-        </div>
-      ))}
+      {data &&
+        data.map((path: string) => (
+          <div className="imageWrapper">
+            <img src={path} alt="" />
+          </div>
+        ))}
     </section>
   );
 };
